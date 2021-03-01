ca930a367ad401a068ac8b4e49908fa250b1afd9
Ardhi Ramadhan <ardhiramadhan98@gmail.com>
2021-03-02 00:37:16 +0700

    From Local Title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>@yield('page_title') | {{ env('app_name') }}</title>
+    <title>@yield('page_title') | {{ env('SUB_NAME') }}</title>
     @if (env('STATUS') === 'DEVELOPMENT')
         <link rel="shortcut icon" href="{{asset('/dev-logo.png')}}" />
     @else
